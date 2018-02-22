359da386ab2a97fc5e5da7ee77d5e4f9a845ed70
_andy.b_ <moonx2006@gmail.com>
2018-02-22 19:53:36 +0100

    completed setup

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from setuptools import setup
 
-setup(
-    name='pylovepdf',
-    version='1.3.0',
-    packages=['pylovepdf', 'pylovepdf.tools'],
-    url='http://www.github.com/AndyBTuttofare',
-    license='LICENSE.md',
-    author='Andrea.Bruschi',
-    author_email='moonx2006@gmail.com',
-    description='ilovepdf.com python API library',
-    long_description=open('README.md').read(),
-    install_requires=['requests==2.18.4', 'urllib3==1.22']
-)
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='pylovepdf',
+      version='1.3.0',
+      description='ilovepdf.com python API library',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+      ],
+      keywords='pdf unlock compress manipulate edit protect pdftojpg jpgtopdf pdfa',
+      url='https://github.com/AndyBTuttofare/pylovepdf',
+      author='Andrea Bruschi',
+      author_email='moonx2006@gmail.com',
+      license='GNU GPLv3',
+      packages=['pylovepdf', 'pylovepdf.tools'],
+      install_requires=['requests', 'urllib3'],
+      include_package_data=True,
+      zip_safe=False)
