c228ea6b1d56d9e9a4a0f77c8596913be5000f83
tgrandje <tgrandje@gmail.com>
2018-06-12 16:26:09 +0200

    Handling proxies

--- pylovepdf/tools/protect.py
+++ pylovepdf/tools/protect.py
@@ -3,8 +3,8 @@ from pylovepdf.task import Task
 
 class Protect(Task):
 
-    def __init__(self, public_key, verify_ssl):
+    def __init__(self, public_key, verify_ssl, proxies):
 
         self.tool = 'protect'
 
-        super(Protect, self).__init__(public_key, True, verify_ssl)
+        super(Protect, self).__init__(public_key, True, verify_ssl, proxies)
