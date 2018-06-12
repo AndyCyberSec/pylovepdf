e963204085ee55442b6c383014740cfa40728045
tgrandje <tgrandje@gmail.com>
2018-06-12 16:26:27 +0200

    Handling proxies

--- pylovepdf/tools/rotate.py
+++ pylovepdf/tools/rotate.py
@@ -3,10 +3,10 @@ from pylovepdf.task import Task
 
 class Rotate(Task):
 
-    def __init__(self, public_key, verify_ssl):
+    def __init__(self, public_key, verify_ssl, proxies):
 
         self.tool = 'rotate'
-        super(Rotate, self).__init__(public_key, True, verify_ssl)
+        super(Rotate, self).__init__(public_key, True, verify_ssl, proxies)
 
 
 
